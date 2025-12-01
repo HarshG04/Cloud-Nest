11da4eda940f8e4050cdc649a64ee6be8a6e1c5c
Aditya56Shankar <122800149+Aditya56Shankar@users.noreply.github.com>
2025-12-02 00:20:39 +0530

    added logger and db connection

--- server/app.js
+++ server/app.js
@@ -1,18 +1,51 @@
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express from 'express'
+import express from 'express';
+import morgan from "morgan";
+import logger from "./logger.js";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+app.use(cookieParser());
+app.set("view engine", "ejs");
+const morganFormat = ":method :url :status :response-time ms";
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 //routes
-import { router } from './routes/user.routes.js'
 
-app.use("/user", router);
-
-export { app }
+import healthRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+app.use("/api/v1/healthcheck",  healthRouter);
+app.use("/api/v1/users", userRouter); // Note: Changed from /user to /users
+app.use(errorHandler) ;
+export { app };
--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
-import express from 'express'
-import connect from './db/index.js'
-import dotenv from 'dotenv'
-import {app} from './app.js'
+import cors from 'cors';
+import dotenv from "dotenv";
+import express from 'express';
+
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+
 dotenv.config({
-  path:'./.env'
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+// let book = [];
+// let id = 1;
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 })
-
-
-const port = process.env.PORT || 7000
-connect().
-then(()=>{
-  app.listen(port, () => {
-            console.log(`app listening on port ${port}`)
-        })
-}).catch((err) => {
-  console.error('Database connection failed:', err)
-})
-
+.catch((error) => {
+    console.log("MongoDB connection failed:", error);
+    process.exit(1);
+});
 
 
