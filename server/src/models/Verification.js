bcb0a9dea6c0cc855e0a2c69f47e30368108dc94
Aditya Shankar <122800149+Aditya56Shankar@users.noreply.github.com>
2025-12-04 22:27:04 +0530

    Delete server directory

--- server/src/models/Verification.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const verificationSchema = new mongoose.Schema({
-  identifier: { type: String, required: true },
-  value: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Verification", verificationSchema);
